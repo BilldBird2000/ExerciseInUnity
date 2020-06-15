42890e2cea44276d602a2e464a022b8916e9d0da
BilldBird2000 <174016204@qq.com>
2020-06-16 00:55:35 +0800

    add  code

--- CardBased/Assets/Scripts/Component/UI_Login.cs
+++ CardBased/Assets/Scripts/Component/UI_Login.cs
@@ -6,10 +6,19 @@ using UnityEngine.UI;
 //新语法
 //1.btnList.AddRange (transform.GetComponentsInChildren<Button> ( ));
 //  List的属性.AddRange(),增加成员,参数有别于.Add()
+
 //2.btnList [ i ].onClick.AddListener (delegate ( ) { OnClickAddBtn (sender.gameObject); });
 //  onClick.AddListener(delegate ( ) { })添加委托参数,解决的问题是在点击时传递参数
 //  delegate ( ) { OnClickAddBtn ( ); } 自定义OnClick函数使其能够传递参数
 //  sender.gameObject组件,定位到当前节点. Component类默认包含了常用组件,可以用 .组件名 的方式访问
+
+//3.非按钮UI资源,比如Image,可以被点击响应的实现方法:(以下3步缺一不可)
+//  using UnityEngine.EventSysten;      //1-引用接口命名空间
+//  public class ImageOnTouch:MonoBehaviour,IPointerClickHandlerq   //2-继承接口IPointerClickHeadler
+//  {
+//      public void OnPointerClick ( PointerEventData eventdata ) { }   //3-实现接口
+//  }
+//  
 
 
 namespace CardBased_V1
@@ -24,7 +33,7 @@ namespace CardBased_V1
         private List<Button> btnList = new List<Button> ( );
         //public Transform [ ] target;
         private Button nextBtn;
-        private GameObject btnNode;
+        private GameObject player = null;
 
         void Start ( )
         {
@@ -34,7 +43,7 @@ namespace CardBased_V1
 
         void Update ( )
         {
-            
+
         }
 
         public void AddButton ( )
@@ -43,53 +52,59 @@ namespace CardBased_V1
             for ( int i = 0; i < btnList.Count; i++ )
             {
                 Button sender = btnList [ i ];
-                if ( sender.gameObject.name != "Next" )
-                {
-                    sender.interactable = true;
-                    btnList [ i ].onClick.AddListener (delegate ( ) { PlayerBtnPress (sender.gameObject); });
-                }
-                else
+                if ( sender.gameObject.name == "Next" )
                 {
                     nextBtn = btnList [ i ];
                     nextBtn.interactable = false;
-                    nextBtn.onClick.AddListener (NextBtnPress);
+                    nextBtn.onClick.AddListener (OnNext);
+
+                }
+                else if ( sender.gameObject.name == "Null" )
+                {
+                    sender.interactable = true;
+                    btnList [ i ].onClick.AddListener (OnNull);
+                }
+                else
+                {
+                    sender.interactable = true;
+                    btnList [ i ].onClick.AddListener (delegate ( ) { OnPlayer (sender.gameObject); });
                 }
             }
 
-
         }
 
-        public void PlayerChoosed ( GameObject node )
+        private void OnPlayer ( GameObject node )
         {
+            Debug.LogFormat ("按钮{0}被点击" , node.name);
+            player = node;
             nextBtn.interactable = true;
-            btnNode = node;
 
         }
 
-
-
-        private void PlayerBtnPress ( GameObject node )
+        private void OnNull ( )
         {
-            Debug.LogFormat ("按钮{0}被点击~" , node.name);
-
-            PlayerChoosed (node);
-
+            if ( player != null )
+            {
+                Debug.LogFormat ("撤销选择对象--------");
+                player = null;
+                nextBtn.interactable = false;
+            }
         }
 
-        private void NextBtnPress ( )
+        private void OnNext ( )
         {
             Debug.LogFormat ("按钮{0}被点击~" , nextBtn.gameObject.name);
 
-            PlayerInit init = btnNode.AddComponent<PlayerInit> ( );
-            init.Init (btnNode);
+            PlayerInit init = player.AddComponent<PlayerInit> ( );
+            init.Init (player);
 
-            Debug.LogFormat ("即将跳转到战斗界面...");
+            Debug.LogFormat ("即将跳转到战斗界面>>>>>>>>");
 
         }
 
 
 
-        //添加点击事件的一种方法,被AddButton代替
+        //添加点击事件的一种方法,被AddButton()代替
         //用onClick.AddListener()添加的方法不能传参,不方便处理多个按钮事件,局限性比较大
         //private Text btnName;
         //private Button btn;
--- CardBased/Assets/Scripts/Method/PlayerInit.cs
+++ CardBased/Assets/Scripts/Method/PlayerInit.cs
@@ -7,12 +7,10 @@ namespace CardBased_V1
 {
     public class PlayerInit : RoleBase
     {
-        //private Button player;
-
         public void Init( GameObject node )
         {
             int hp = playerInfo [ node.name ];
-            Debug.LogFormat ("初始化角色{0},Hp:{1}" , node.name , hp);
+            Debug.LogFormat ("++++++++初始化角色{0},Hp:{1}" , node.name , hp);
         }
 
 
